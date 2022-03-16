c62a3004a096ec963066645105dcb80782c85595
Candice <candicebranchereau@gmail.com>
2022-03-16 15:43:10 +0100

    [main] add simple menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include "guessTheNumber/play_guess_the_number.h"
 #include "hangman/play_hangman.h"
+#include "common/get_input_from_user.h"
+#include <iostream>
+
+void showMenu();
 
 int main()
 {
-    //play_guess_the_number();
-    play_hangman();
+    bool quit = false;
+
+    while(!quit)
+    {
+        showMenu();
+        char choice = get_input_from_user<char>();
+        switch(choice)
+        {
+            case '1': play_guess_the_number();
+                break;
+            case '2': play_hangman();
+                break;
+            case 'q':
+            case 'Q': 
+                return 0;
+                break;
+            default: std::cout << "Sorry I don't know that command !" << std::endl;
+        }
+    }
+
+}
+
+void showMenu() {
+    std::cout << "What do you want to do ?" << std::endl;
+    std::cout << "1: Play 'Guess the number'" << std::endl;
+    std::cout << "2: Play 'Hangman'" << std::endl;
+    std::cout << "q : Quit" << std::endl;
 }
 
