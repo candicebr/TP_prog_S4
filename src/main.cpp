83b824ad7dd25d8169f6bc0c6a3792cacbd56c58
Candice <candicebranchereau@gmail.com>
2022-04-13 17:01:13 +0200

    [p6] add p6 submodule + example

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,19 @@
 #include "menu.h"
 #include <iostream>
 
+#include "../p6/include/p6/p6.h"
 
 int main() {
-    getMenu();
+    //getMenu();
+
+    auto ctx = p6::Context{{1280, 720, "Noughts and Crosses"}}; // Create a window
+
+    ctx.update = [&]() {                    // Define the function that will be called in a loop once you call ctx.start()
+        ctx.background({0.5f, 0.3f, 0.8f});
+        ctx.square(p6::Center{ctx.mouse()},
+                   p6::Radius{0.3f});
+    };
+    ctx.start(); 
 
     return 0;
 }
